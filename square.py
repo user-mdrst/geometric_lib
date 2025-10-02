822c6d0750c3e1d97cbbf982e67ad382df1b2dcb
Ruslan <mrkadyrovruslan@mail.ru>
2025-10-03 00:19:51 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,7 +1,36 @@
+def area(a)
+    '''
+    Площадь квадрата
 
-def area(a):
+    Args:
+        a (int or float): сторона квадрата
+
+    Returns:
+        int or float: площадь квадрата
+
+    Examples:
+        >>> area(4)
+        16
+        >>> area(3.5)
+        12.25
+    '''
     return a * a
 
 
 def perimeter(a):
+    '''
+    Периметр квадрата
+
+    Args:
+        a (int): сторона квадрата
+
+    Returns:
+        int or float: периметр квадрата
+
+    Examples:
+        >>> perimeter(5)
+        20
+        >>> perimeter(3.5)
+        14
+    '''
     return 4 * a
